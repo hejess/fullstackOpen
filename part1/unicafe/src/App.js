8f51a98dad032ae21bb984070b7fda49680f0459
Jessie He <jessie.he@deakin.edu.au>
2023-08-01 20:12:14 +1000

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,6 +13,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let all = good+neutral+bad
+  let average = (good - bad) / all
+  let positive = good / all
 
   return (
     <div>
@@ -24,6 +27,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all} </p>
+      <p>average {average} </p>
+      <p>positive {positive*100}% </p>
     </div>
    
   )
