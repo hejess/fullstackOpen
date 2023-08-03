7e6efc83dc33a4c1e6680922785a6bdedc53ceca
Jessie He <jessie.he@deakin.edu.au>
2023-08-03 19:25:13 +1000

    fetch data from json-server using axios with an Effect hook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 const Person = ({ person }) => (
   <tr>
     <td>{person.name}</td>
@@ -20,11 +20,13 @@ const PersonForm = ({
       value={newName}
       handleChange={handleNameChange}
     />
+
     <InputWithText
       text={'number:'}
       value={newNum}
       handleChange={handleNumChange}
     />
+
     <div>
       <button type='submit'>add</button>
     </div>
@@ -53,15 +55,17 @@ const InputWithText = ({ text, value, handleChange }) => (
 )
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [newFilterStr, setNewFilterStr] = useState('')
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }
+  useEffect(hook, []) // By default, effects run after every completed render
 
   const addPerson = event => {
     event.preventDefault()
